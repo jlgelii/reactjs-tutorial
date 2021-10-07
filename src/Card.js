00f553a7c9bc3e06c1d0ea8a5c186b20f0aa32d3
Jeff Geli <90401597+TFIjgeli@users.noreply.github.com>
2021-10-08 00:42:27 +0800

    Initial commit

---
+++ src/Card.js
@@ -0,0 +1,14 @@
+import React from "react"
+
+const Card = () => {
+    return(
+        <div className="users-card">    
+            <h1>Users Card</h1>
+            <div>Name: Jeff</div>
+            <div>Age: 31</div>
+            <div>Gender: Male</div>
+        </div>
+    )
+}
+
+export default Card
